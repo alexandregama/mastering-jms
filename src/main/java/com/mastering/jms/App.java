94994674067f6f487e9675e303b7f850e1fd2b21
alexandregama <alexandre.gama.lima@gmail.com>
2016-01-31 00:25:47 -0200

    Initial commit

---
+++ src/main/java/com/mastering/jms/App.java
@@ -0,0 +1,13 @@
+package com.mastering.jms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
