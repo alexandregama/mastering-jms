b0fb7eb654e72ffb29072a8159f22247244ba1c3
alexandregama <alexandre.gama.lima@gmail.com>
2016-03-02 21:18:20 -0300

    Creates a Listener using Message Driven to generate Fiscal Data

---
+++ src/main/java/com/mastering/jms/topic/subscriber/FiscalDataTopicMessageDrivenListener.java
@@ -0,0 +1,24 @@
+package com.mastering.jms.topic.subscriber;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "jms/TOPIC.FISCAL.DATA"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic")
+})
+public class FiscalDataTopicMessageDrivenListener implements MessageListener {
+
+	public void onMessage(Message message) {
+		TextMessage text = (TextMessage) message;
+		try {
+			System.out.println("Message received to generate Fiscal Data: " + text.getText());
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+}
